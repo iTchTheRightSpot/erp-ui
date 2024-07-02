ce1f7f6756bdad68d8dfae6e6ce233e1f12106d2
emmanuelU17 <emmanueluluabuike@gmail.com>
2024-07-02 05:19:45 -0400

    improved overflow

--- src/app/employee-front/user/user-card/user-card.component.ts
+++ src/app/employee-front/user/user-card/user-card.component.ts
@@ -158,7 +158,7 @@ import { UserDetailsComponent } from '@/app/employee-front/user/user-details/use
           </button>
         </div>
 
-        <div class="w-full max-h-4/5 overflow-y-scroll mx-auto">
+        <div class="w-full max-h-[500px] xl:max-h-fit overflow-y-scroll mx-auto">
           <app-user-details
             [employeeId]="employeeId()"
             [image]="image()"
